511795164afa972d98d604bb3e087b0dc3ca48a5
ilhanigit <ibrahimilhan1999@gmail.com>
2025-01-09 12:33:27 +0000

    added dev-k8s-terraform  for kubernetes infrastructure

---
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -0,0 +1,154 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "sec-gr-k8s" {
+  default = "petclinic-k8s-sec-gr"
+}
+
+data "aws_vpc" "name" {
+  default = true
+}
+
+resource "aws_security_group" "k8s-sec-gr" {
+  name = var.sec-gr-k8s
+  vpc_id = data.aws_vpc.name.id
+  
+  tags = {
+    Name = var.sec-gr-k8s
+  }
+  
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 30000
+    protocol = "tcp"
+    to_port = 32767
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 6443
+    protocol = "tcp"
+    to_port = 6443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    self = true
+  }
+
+  egress {
+    from_port = 0
+    protocol = -1
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role" "petclinic-master-server-s3-role" {
+  name = "petclinic-master-server-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "petclinic-s3-policy" {
+  role = aws_iam_role.petclinic-master-server-s3-role
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+resource "aws_iam_instance_profile" "petclinic-master-server-profile" {
+  name = "petclinic-master-server-profile"
+  role = aws_iam_role.petclinic-master-server-s3-role.name
+}
+
+resource "aws_instance" "kube-master" {
+  ami = "ami-005fc0f236362e99f"
+  instance_type = "t3a.medium"
+  vpc_security_group_ids = [aws_security_group.k8s-sec-gr.id]
+  key_name = instancekey
+  iam_instance_profile = aws_iam_instance_profile.petclinic-master-server-profile.name
+  subnet_id = "subnet-0616125ac1800d1a8"
+  availability_zone = "us-east-1a"
+
+  tags =  {
+    Name = "kube-master"
+    Project = "tera-kube-ans"
+    Role = "master"
+    id = "1"
+    environment = "dev"
+  } 
+}
+
+resource "aws_instance" "worker-1" {
+  ami = "ami-005fc0f236362e99f"
+  instance_type = "t3a.medium"
+  vpc_security_group_ids = [aws_security_group.k8s-sec-gr.id]
+  key_name = instancekey
+  subnet_id = "subnet-0616125ac1800d1a8"
+  availability_zone = "us-east-1a"
+
+  tags =  {
+    Name = "worker-1"
+    Project = "tera-kube-ans"
+    Role = "worker"
+    id = "1"
+    environment = "dev"
+  } 
+}
+
+resource "aws_instance" "worker-2" {
+  ami = "ami-005fc0f236362e99f"
+  instance_type = "t3a.medium"
+  vpc_security_group_ids = [aws_security_group.k8s-sec-gr.id]
+  key_name = instancekey
+  subnet_id = "subnet-0616125ac1800d1a8"
+  availability_zone = "us-east-1a"
+
+  tags =  {
+    Name = "worker-2"
+    Project = "tera-kube-ans"
+    Role = "worker"
+    id = "1"
+    environment = "dev"
+  } 
+}
+
+output "kube-master-ip" {
+  value = aws_instance.kube-master.public_ip
+  sensitive = false
+  description = "public ip of kube-master machine"
+}
+
+output "worker-1-ip" {
+  value = aws_instance.worker-1.public_ip
+  sensitive = false
+  description = "public ip of the worker-1 machine"
+}
+
+output "worker-2-ip" {
+  value = aws_instance.worker-2.public_ip
+  sensitive = false
+  description = "public ip of the worker-2 machine"
+}
