0999d1a82f576bd41014520a2197a74be42190ec
ibrahim ilhan <ibrahimilhan1999@gmail.com>
2025-01-16 14:18:18 +0300

    Update deploy_app_on_qa_environment.sh

--- jenkins/deploy_app_on_qa_environment.sh
+++ jenkins/deploy_app_on_qa_environment.sh
@@ -1,11 +1,7 @@
 echo 'Deploying App on Kubernetes'
 envsubst < k8s/petclinic_chart/values-template.yaml > k8s/petclinic_chart/values.yaml
 sed -i s/HELM_VERSION/${BUILD_NUMBER}/ k8s/petclinic_chart/Chart.yaml
-<<<<<<< HEAD
-AWS_REGION=$AWS_REGION helm repo add stable-petclinic s3://petclinic-helm-charts-<put-your-name>/stable/myapp/ || echo "repository name already exists"
-=======
 AWS_REGION=$AWS_REGION helm repo add stable-petclinic s3://petclinic-helm-charts-abraham/stable/myapp/ || echo "repository name already exists"
->>>>>>> feature/msp-21
 AWS_REGION=$AWS_REGION helm repo update
 helm package k8s/petclinic_chart
 AWS_REGION=$AWS_REGION helm s3 push --force petclinic_chart-${BUILD_NUMBER}.tgz stable-petclinic
